ed4c13bfe570fff4fe2762d2d07824abc38cd591
SamuelAntunes2006 <samuel.asantos@sptech.school>
2025-06-06 11:53:03 -0300

    Adicionando model do login

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -12,6 +12,19 @@ function cadastrar(apelido, senha) {
     return database.executar(instrucaoSql);
 }
 
+function autenticar(apelido, senha){
+
+     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, senha);
+
+     var instrucaoSql = `
+    SELECT * FROM usuario WHERE apelido = ${apelido}
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
-    cadastrar
+    cadastrar,
+    autenticar
 };
